19ca287b097308fcce530771a51e795e0d6bfa8e
Joybitu <97351019+Joybitu@users.noreply.github.com>
2022-01-08 18:51:21 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,33 +1,48 @@
 const personagem = document.getElementById("personagem");
 const quadrado = document.getElementById("quadrado");
+let gameOver = document.getElementById("gameOver");
+let score = document.getElementById("score");
 
-function jump(){
-    if(personagem.classList != "jump"){
-      personagem.classList.add("jump");
-    
-    setTimeout(function(){
-        personagem.classList.remove("jump");
-    }, 300);
-    }
+let interval = null;
+let playerScore = 0;
+
+
+let scoreCounter =()=> {
+    playerScore=playerScore + 20;
+    score.innerHTML = "Score:" + playerScore;
 }
+
+
+interval = setInterval(scoreCounter,200);
+
+window.addEventListener("keydown", (e)=>{
+
+    if(e.key == "w")
+      if(personagem.classList != "jump")
+      { 
+            personagem.classList.add("jump");
+      
+    
+             setTimeout(()=>{
+                 personagem.classList.remove("jump");
+            },300);
+      }
+         
+});
+
 
 let testarColisao = setInterval( function (){
 
-    let topoPersonagem = parseInt(
-     window.getComputedStyle(personagem).getPropertyValue("top")
+    let topPersonagem = parseInt(
+getComputedStyle(personagem).getPropertyValue("top")
     );
     let EsquerdaQuadrado = parseInt(
-        window.getComputedStyle(quadrado).getPropertyValue("left")
-       );
+        getComputedStyle(quadrado).getPropertyValue("left")
+    );
 
-       if(EsquerdaQuadrado < 50 && EsquerdaQuadrado > 0 && topoPersonagem >= 140){
-     
+    if(topPersonagem >= 140 && EsquerdaQuadrado > 0 && EsquerdaQuadrado < 50){
         alert("Game over!")
             
     }
+},10);
 
-}, 10);
-
-document.addEventListener("keydown", function (event) {
-    jump();
-  });
